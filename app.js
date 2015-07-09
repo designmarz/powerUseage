22b9013074f4a79ead3455108f648aa540138e91
Nicholas Marazzo <designmarz@gmail.com>
2015-07-08 23:37:56 -0700

    cleaning up files

--- app.js
+++ app.js
@@ -1,23 +1,5 @@
 $(document).ready(function() {
 
-	// var fuzzyOptions = {
-	// 	searchClass: "fuzzy-search",
-	// 	location: 0,
-	// 	distance: 1000,
-	// 	threshold: 0.9,
-	// 	multiSearch: true
-	// };
-
-
-	// var options = {
-	// 	searchClass: "fuzzy-search",
-	// 	valueNames: [ 'panelName', 'circuit','endUse', 'userLabel', 'load', 'min15', 'hour1', 'hour6', 'hour24' ],
-	// 	plugins: [
-	// 	ListFuzzySearch()
-	// 	]
-	// };
-
-	// list = new List('useageData', options);
 
 	$( "span.ui-slider-handle.ui-state-default.ui-corner-all").mousedown(function() {
 		$( "#amount" ).click();
@@ -28,39 +10,29 @@ $(document).ready(function() {
 		$(".sorting").removeClass("active")
 		$(".sorting_asc").removeClass("active")
 		$(".sorting_dsc").removeClass("active")
-		// console.log("toggle class?")
-		// console.log(this)
 		$(this).addClass("active")
 	});
 
-// $('#useageData').DataTable();
+	$(".sorting_asc").click(function(event) {
+		$(".sorting").removeClass("active")
+		$(".sorting_asc").removeClass("active")
+		$(".sorting_dsc").removeClass("active")
+		$(this).addClass("active")
+	});
 
-// $.fn.dataTable.ext.search.push(
-//     function( settings, data, dataIndex ) {
-//         var min = parseInt( $('#minAmount').val(), 10 );
-//         var max = parseInt( $('#maxAmount').val(), 10 );
-//         var age = parseFloat( data[3] ) || 0; // use data for the age column
- 
-//         if ( ( isNaN( min ) && isNaN( max ) ) ||
-//              ( isNaN( min ) && age <= max ) ||
-//              ( min <= age   && isNaN( max ) ) ||
-//              ( min <= age   && age <= max ) )
-//         {
-//             return true;
-//         }
-//         return false;
-//     }
-// );
-
+	$(".sorting_dsc").click(function(event) {
+		$(".sorting").removeClass("active")
+		$(".sorting_asc").removeClass("active")
+		$(".sorting_dsc").removeClass("active")
+		$(this).addClass("active")
+	});
 
 $.fn.dataTableExt.afnFiltering.push(
     function( oSettings, aData, iDataIndex ) {
         var iMin = parseFloat(document.getElementById('minAmount').innerHTML);
         var iMax = parseFloat(document.getElementById('maxAmount').innerHTML);
         iVersion = parseFloat(aData[8]) == "-" ? 0 : parseFloat(aData[8]);
-        // console.log(iVersion);
-        // console.log(aData);
-        // console.log(parseFloat(aData[8]));
+
         if ( iMin == "" && iMax == "" )
         {
             return true;
@@ -88,7 +60,7 @@ $.fn.dataTableExt.afnFiltering.push(
     var oTable = $('#useageData').dataTable(
     	{
         "bPaginate": false,
-        "bLengthChange": true,
+        "bLengthChange": false,
         "bFilter": true,
         "bSort": true,
         "bInfo": true,
@@ -96,17 +68,33 @@ $.fn.dataTableExt.afnFiltering.push(
     }
     	);
      
-    /* Add event listeners to the two range filtering inputs */
+
     $('span.ui-slider-handle.ui-state-default.ui-corner-all').mouseup( function() { oTable.fnDraw(); } );
     $('span.ui-slider-handle.ui-state-default.ui-corner-all').mousedown( function() { oTable.fnDraw(); } );
-    // $('#maxAmount').mouseup( function() { oTable.fnDraw(); } );
+  
+$(function() {
+
+  $( "#slider-range" ).slider({
+    range: true,
+    min: 0.0,
+    max: 200.0,
+    values: [ 0.0, 150.0 ],
+    slide: function( event, ui ) {
+      $( "#amount" ).val( ui.values[ 0 ] + "kWh - " + ui.values[ 1 ]+ " kWh" );
+      $( "#minAmount" ).html( $( "#slider-range" ).slider( "values", 0 ));
+      $( "#maxAmount" ).html( $( "#slider-range" ).slider( "values", 1 ));
+
+    }
+  });
+
+  $( "#amount" ).val( $( "#slider-range" ).slider( "values", 0 ) +
+    " kWh - " + $( "#slider-range" ).slider( "values", 1 ) + " kWh");
+  $( "#minAmount" ).html( $( "#slider-range" ).slider( "values", 0 ));
+  $( "#maxAmount" ).html( $( "#slider-range" ).slider( "values", 1 ));
 
 
-    // Event listener to the two range filtering inputs to redraw on input
-    // $('#minAmount, #maxAmount').mouseup( function() {
-        // oTable.fnDraw();
-    // } );
 
+});
 
 
 });
